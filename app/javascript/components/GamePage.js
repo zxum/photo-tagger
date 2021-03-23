eb34e5cabb47988976eb0bc4ca25319742b05a51
zxum <zion.um@outlook.com>
2021-03-23 10:55:38 -0500

    Add timer and end game functionality

--- app/javascript/components/GamePage.js
+++ app/javascript/components/GamePage.js
@@ -13,7 +13,32 @@ function GamePage(props) {
   let [selectionPos, setSelectionPos] = useState({x:0,y:0})
   let [contextMenuHidden, setContextMenuHidden] = useState(false)
   let requestImageFiles = require.context('../../assets/images',true,/.jpg$/)
+  
+  let [second, setSecond] = useState('00')
+  let [minute, setMinute] = useState('00')
+  let [isActive, setActive] = useState(false)
+  let [counter, setCounter] = useState(0)
+ 
+  useEffect(() => {
+    let intervalId;
 
+    if (isActive) {
+      intervalId = setInterval(() => {
+        const secondCounter = counter % 60;
+        const minuteCounter = Math.floor(counter / 60);
+
+        const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}`: secondCounter;
+        const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}`: minuteCounter;
+
+        setSecond(computedSecond);
+        setMinute(computedMinute);
+
+        setCounter(counter => counter + 1);
+      }, 1000)
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, counter])
 
   useEffect(()=>{
     {fetch(`../api/v1/pictures/${id}.json`)
@@ -21,17 +46,21 @@ function GamePage(props) {
       .then(data => {
         setPicture(data.data.attributes)
         setCharacters(data.included)
+        setActive(true)
       }) 
     }
   },[])
 
   const handleImageClick = (event) => {
+    let offset
+    (id == 1 ? offset = 4 : offset = 7)
+    console.log(offset)
     event.preventDefault()
     // console.log(event)
     let {pageX,pageY} = event
     let {scrollHeight,scrollWidth} = event.target
     let xposPercent = pageX/scrollWidth * 100 
-    let yposPercent = (pageY/scrollHeight * 100) - 4 
+    let yposPercent = (pageY/scrollHeight * 100) - offset
     let xpos = pageY
     let ypos = pageX
 
@@ -59,21 +88,37 @@ function GamePage(props) {
                     .then(response => {
                       let data = response.data.data 
                       setCharacters(data)
-                      
+                      let foundCharacters = data.filter(char => char.attributes.found == 't')
+                      console.log(characters)
+                      console.log(foundCharacters)
+                      if (foundCharacters.length === 5) {
+                        handleWin() 
+                      }
                     })
       }
 
       setContextMenuHidden(!contextMenuHidden)
+
     } catch (error) {
       console.error(error)
     }
+  }
+
+  const handleWin = async () => {
+    setActive(false)
+    await axios.patch(`/api/v1/scoreboard/${id}`, { score: `${minute}:${second}`}) 
+                .then(response => {
+                  console.log(response)
+                })
   }
 
   return (
     <div className="app-main">
       <Navbar picture={picture} 
               characters={characters} 
-              requestImageFiles={requestImageFiles}/> 
+              requestImageFiles={requestImageFiles}
+              second={second}
+              minute={minute}/> 
       {contextMenuHidden && (
         <ContextMenu 
               xPos={menuPos.x} 
